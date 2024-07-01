568b7bbb2f6ef9493b93c2740e138961b8247a00
Veronicchkamin <veronichka.minchonak@gmail.com>
2024-07-01 11:36:16 +0200

    add

--- next try1/Program.cs
+++ next try1/Program.cs
@@ -2,3 +2,15 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Who arey you?");
+
+int a = 10;
+int b = 20;
+int c = a + b;
+
+Console.Write(c);
+
+
+if (a == 20);
+{ 
+Console.WriteLine("yessss");
+}
