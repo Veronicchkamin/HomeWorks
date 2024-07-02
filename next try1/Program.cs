85aa4f682573c52f34eea715584f7d41d600819e
Veronicchkamin <128373932+Veronicchkamin@users.noreply.github.com>
2024-07-02 23:58:02 +0200

    Update Program.cs

--- next try1/Program.cs
+++ next try1/Program.cs
@@ -1,18 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Who arey you?");
+namespace Calculator
 
-int a = 10;
-int b = 20;
-int c = a + b;
+{
+    internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Choose an operation:");
+        Console.WriteLine("Choose + for addition");
+        Console.WriteLine("Choose - for subtraction");
+        Console.WriteLine("Choose * for multiplication");
+        Console.WriteLine("Choose / for division");
 
-Console.Write(c);
+        switch (Console.ReadLine())
+        {
+            case "+":
+                Console.WriteLine(firstNumber + secondNumber);
+                break;
+            case "":
+                Console.WriteLine(firstNumber - secondNumber);
+                break;
+            case "3":
+                Console.WriteLine(firstNumber * secondNumber);
+                break;
+            case "4":
+                Console.WriteLine(firstNumber / secondNumber);
+                break;
+            default:
+                Console.WriteLine("You haven't made your choice");
+                break;
+
+        }
 
 
-if (a == 20);
-{ 
-Console.WriteLine("yessss");
+    }
 }
-
-vhvhvh
+}
