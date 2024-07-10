1fd2dff2af9c754f8840838a68ba8a94aa8270be
Veronicchkamin <128373932+Veronicchkamin@users.noreply.github.com>
2024-07-11 00:06:23 +0200

    Update Calc.cs

--- next try1/Calc/Calc.cs
+++ next try1/Calc/Calc.cs
@@ -1,1 +1,43 @@
+// See https://aka.ms/new-console-template for more information
 
+namespace Calculator
+
+{
+    internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Choose an operation:");
+        Console.WriteLine("Choose + for addition");
+        Console.WriteLine("Choose - for subtraction");
+        Console.WriteLine("Choose * for multiplication");
+        Console.WriteLine("Choose / for division");
+
+        switch (Console.ReadLine())
+        {
+            case "+":
+                Console.WriteLine(firstNumber + secondNumber);
+                break;
+            case "":
+                Console.WriteLine(firstNumber - secondNumber);
+                break;
+            case "3":
+                Console.WriteLine(firstNumber * secondNumber);
+                break;
+            case "4":
+                Console.WriteLine(firstNumber / secondNumber);
+                break;
+            default:
+                Console.WriteLine("You haven't made your choice");
+                break;
+
+        }
+
+
+    }
+}
+}
