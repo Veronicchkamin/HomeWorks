8717b2e507742c96ee2b2b44aad8478038a63a40
Veronicchkamin <128373932+Veronicchkamin@users.noreply.github.com>
2024-07-16 17:32:49 +0200

    Update Counting words with numbers.cs

--- LearningC#/Homework 5/Counting words with numbers.cs
+++ LearningC#/Homework 5/Counting words with numbers.cs
@@ -1,1 +1,29 @@
+string ConsoleText = "Hi111111111! My na111me i76534s Veroni66543222ka.";
+var ArrayWords = ConsoleText.Split(new char[]
+{
+    ' ',',','!','.' }, StringSplitOptions.RemoveEmptyEntries);
+var counter = 0;
+var result = string.Empty;
+var maxCounter = 0;
+
+foreach (var word in ArrayWords)
+{
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (char.IsDigit(word[i]))
+        {
+            counter++;
+        }
+    }
+    if (maxCounter < counter)
+    {
+        result = word;
+        maxCounter = counter;
+    }
+
+    counter = 0;
+
+}
+
+Console.WriteLine(result);
 
